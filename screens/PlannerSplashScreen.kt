da813619864e93ac5f3347d8fc003d395945ccbf
Ariana23 <arianavargas235@hotmail.com>
2023-11-07 11:00:23 -0300

    Agregando proyectito (:

---
+++ screens/PlannerSplashScreen.kt
@@ -0,0 +1,11 @@
+package com.example.planner21.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun PlannerSplashScreen(navController: NavController){
+    Text(text = "BIENVENIDE SEÑORRR")
+
+}
---
+++ screens/login/PlannerLoginScreen.kt
@@ -0,0 +1,11 @@
+package com.example.planner21.screens.login
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun PlannerLoginScreen(navController: NavController){
+    Text(text = "logiin")
+
+}
